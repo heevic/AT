cbaf965921f96932222df0dd4446e535f140f1a5
heevic <dbgmltmd0317@gmail.com>
2023-09-04 06:00:51 +0900

    style: 모바일 네비게이션 스타일 수정

--- src/components/UI/Nav/GlobalNavItems.tsx
+++ src/components/UI/Nav/GlobalNavItems.tsx
@@ -1,34 +1,52 @@
 'use client';
-import React, {useState} from 'react';
-import styles from "./Navbar.module.scss";
-import Link from "next/link";
-import {usePathname} from "next/navigation";
-import {useSession} from "next-auth/react";
-import {MenuItemProps} from '@/types/Navigation';
+import React, { useState } from 'react';
+import styles from './Navbar.module.scss';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { MenuItemProps, MenuItem } from '@/types/Navigation';
 
-const GlobalNavItems = ({gblMenuItems}: MenuItemProps) => {
-    const {data: session} = useSession();
+const GlobalNavItems = ({ gblMenuItems }: MenuItemProps) => {
+    const { data: session } = useSession();
     const currentRoute = usePathname();
-    const [showSubMenu, setShowSubMenu] = useState(false);
-
-    const handleMouseEnter = () => setShowSubMenu(true);
-    const handleMouseLeave = () => setShowSubMenu(false);
 
     return (
-        <ul className={styles.navMenu} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
-            {/* Global Navigation : 메인 네비게이션 아이템 렌더링 */}
+        <ul className={styles.navMenu}>
             {gblMenuItems.map((item) => {
-                const isActive = currentRoute === item.route || (item.subMenu && item.subMenu.some(subItem => currentRoute === subItem.route));
+                const isActive = currentRoute === item.route || (item.subMenu?.some(subItem => currentRoute === subItem.route));
                 const myPageLink = session?.user?._id ? `/user/mypage/${session.user.name}` : '/login';
+                const hasSubMenu = item.subMenu && item.subMenu.length > 0;
+                const SubMenuhasSubMenu = item.subMenu && item.subMenu.some(subItem => subItem.subMenu && subItem.subMenu.length > 0);
 
                 return (
-                    <li className={styles.navItem} key={item.route}>
+                    <li
+                        className={`${styles.navItem}`}
+                        key={item.route}
+                    >
                         <Link
-                            href={item.title === "마이페이지" ? myPageLink : item.route}
+                            href={item.title === '마이페이지' ? myPageLink : item.route}
                             className={`${styles.navLink} ${isActive ? styles.activeLink : styles.nonActiveLink}`}
                         >
                             {item.title}
                         </Link>
+                        {hasSubMenu && (
+                            <ul className={styles.subMenu}>
+                                {item.subMenu?.map(subItem => (
+                                    <li key={subItem.route}>
+                                        <Link href={subItem.route}>{subItem.title}</Link>
+                                        {SubMenuhasSubMenu && subItem.subMenu && subItem.subMenu.length > 0 && (
+                                            <ul className={styles.subSubMenu}>
+                                                {subItem.subMenu.map(subSubMenu => (
+                                                    <li key={subSubMenu.route}>
+                                                        <Link href={subSubMenu.route}>{subSubMenu.title}</Link>
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        )}
+                                    </li>
+                                ))}
+                            </ul>
+                        )}
                     </li>
                 );
             })}
--- src/components/UI/Nav/GlobalNavbarComponent.tsx
+++ src/components/UI/Nav/GlobalNavbarComponent.tsx
@@ -1,24 +1,24 @@
 'use client'
-import React, {useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from './Navbar.module.scss';
-
+import { useSession } from "next-auth/react";
 import GlobalNavItems from "@/components/UI/Nav/GlobalNavItems";
-import {useSession} from "next-auth/react";
+import UserModal from "@/components/UI/Nav/IsUserStatusModalMenu/IsUserStatusModalMenu";
+import AppLink from "@/components/UI/Link/AppLink";
 import Link from "next/link";
 import Image from "next/image";
-import SvgIconComponent from "@/components/SvgIconComponent";
-import IsUserStatusModalMenu from "@/components/UI/Nav/IsUserStatusModalMenu/IsUserStatusModalMenu";
-import AppLink from "@/components/UI/Link/AppLink";
 import NavigationLogo from "../../../../public/img/home-bg-Transparent.png";
-import {FaRegCircleUser} from "react-icons/fa6";
-import {BsBell} from "react-icons/bs";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { BsBell } from "react-icons/bs";
 import GLOBAL_NAV from "@/data/data-global-nav.json";
-import {MenuItem} from '@/types/Navigation';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineCancel } from "react-icons/md";
+import { MenuItem } from '@/types/Navigation';
 
 
-const GlobalNavbarComponent = () => {
+const GlobalNavbar = () => {
     /* [Client] 유저 세션 사용 */
-    const {data: session} = useSession();
+    const { data: session } = useSession();
     const [gblMenuItems, setGlbMenuItems] = useState<MenuItem[]>(GLOBAL_NAV.ITEMS);
 
     /* 사용자가 로그인하거나 로그아웃할 때마다 setGlbMenuItems 업데이트 */
@@ -27,7 +27,7 @@ const GlobalNavbarComponent = () => {
         if (session?.user?._id) {
             setGlbMenuItems((prevMenuItems) => {
                 return prevMenuItems.map((item) =>
-                    item.title === 'Dashboard' ? {...item, route: `/user/${session.user._id}`} : item
+                    item.title === 'Dashboard' ? { ...item, route: `/user/${session.user._id}` } : item
                 );
             });
         }
@@ -36,25 +36,24 @@ const GlobalNavbarComponent = () => {
         else {
             setGlbMenuItems((prevMenuItems) =>
                 prevMenuItems.map((item) =>
-                    item.title === 'Dashboard' ? {...item, route: '/'} : item
+                    item.title === 'Dashboard' ? { ...item, route: '/' } : item
                 )
             );
         }
     }, [session]);
 
-
     /* [State] 모달 클릭 여부 */
-    const [isMenClicked, setMenuClicked] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const modalRef = useRef<HTMLElement | null>(null);
 
     /* 함수 실행시 State false -> true */
-    const setIsUserModalClicked = () => setMenuClicked(!isMenClicked);
+    const setIsUserModalClicked = () => setIsModalOpen(!isModalOpen);
 
     /*  클릭 이벤트를 감지하여 모달을 닫음 */
     useEffect(() => {
         const handleClickOutside = (e: MouseEvent) => {
             if (modalRef.current && !modalRef.current.contains(e.target as Node)) {
-                setMenuClicked(false);
+                setIsModalOpen(false);
             }
         };
 
@@ -65,14 +64,13 @@ const GlobalNavbarComponent = () => {
         return () => document.removeEventListener("mousedown", handleClickOutside);
     }, []);
 
-
     /* 관리자 페이지 렌더링 훅(useEffect) */
     useEffect(() => {
         const newGlbMenuItems = [...GLOBAL_NAV.ITEMS];
 
         /* 관리자인 경우 메뉴에 관리자 페이지를 추가 */
         if (session?.user?.role === 'admin') {
-            newGlbMenuItems.push({title: 'Admin', route: '/admin'});
+            newGlbMenuItems.push({ title: 'Admin', route: '/admin' });
         }
 
         setGlbMenuItems(newGlbMenuItems);
@@ -81,43 +79,32 @@ const GlobalNavbarComponent = () => {
 
     /* [State] 반응형 (모바일 768) */
     const [isResponsiveOpen, setIsResponsiveOpen] = useState<boolean>(false);
+    const [isHamburgerClicked, setIsHamburgerClicked] = useState<boolean>(false);
+
     /* Navbar 아이콘 클릭시 반응형 메뉴 열고 닫기 */
-    const isOnClickNavbar = (e: React.MouseEvent<SVGElement, MouseEvent>) => {
+    const handleHamburgerClick = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
         e.preventDefault();
-        setIsResponsiveOpen(isResponsiveOpen => !isResponsiveOpen);
-        console.log('[State] 반응형 (모바일 768) : ', isResponsiveOpen)
+        setIsResponsiveOpen(!isResponsiveOpen);
+        setIsHamburgerClicked(!isHamburgerClicked); // 햄버거 클릭 여부를 토글
+        console.log('[State] 반응형 (모바일 768) : ', isResponsiveOpen);
     }
+
 
     /* 렌더링 */
     return (
         <header className={styles.header}>
             <nav className={styles.nav} ref={modalRef}>
                 {/* 네비게이션 로고 */}
-                <div className={styles['logo-wrapper']}>
-                    <Link className={styles['navbar-logo']} href={'/'}>
-                        <Image src={NavigationLogo} width={220.79} height={17} alt="어택 로고 이미지"/>
+                <div className={styles.logo}>
+                    <Link href={'/'}>
+                        <Image src={NavigationLogo} width={220.79} height={17} alt="어택 로고 이미지" />
                     </Link>
                 </div>
 
-                <div className={styles.navLinkWrap}>
-                    {/* 모바일 반응형 메뉴 */}
-                    <div className={styles['responsive-menu']}>
-                        {!isResponsiveOpen ? (
-                            <SvgIconComponent width={25} height={25}
-                                              svgPath={'M19.5 5.25l-7.5 7.5-7.5-7.5m15 6l-7.5 7.5-7.5-7.5'}
-                                              onClick={isOnClickNavbar}/>
-                        ) : (
-                            <SvgIconComponent width={25} height={25}
-                                              svgPath={'M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5'}
-                                              onClick={isOnClickNavbar}/>
-                        )}
-                        {isResponsiveOpen &&
-                            <GlobalNavItems gblMenuItems={gblMenuItems}/>
-                        }
-                    </div>
+                {/* 데스크탑 네비게이션 메뉴 */}
+                <GlobalNavItems gblMenuItems={gblMenuItems} />
 
-                    {/* 데스크탑 네비게이션 메뉴 */}
-                    <GlobalNavItems gblMenuItems={gblMenuItems}/>
+                <div className={styles.navLinkWrap}>
 
                     {/* 세션 관련 드롭다운 메뉴 */}
                     <div className={styles.navSession}>
@@ -140,19 +127,80 @@ const GlobalNavbarComponent = () => {
 
                             {/* 로그인 상태에서 세션 관련 요소 */}
                             {session?.user &&
-                                <div className={styles.userSessionWrap} onClick={setIsUserModalClicked}>
-                                    <BsBell/>
-                                    <FaRegCircleUser/>
-                                    {/* 클릭시 DropdownMenu 노출 */}
-                                    {isMenClicked && <IsUserStatusModalMenu session={session}/>}
+                                <div className={styles.userSessionWrap}>
+                                    <div className={styles.alarm}>
+                                        <BsBell />
+                                    </div>
+                                    <div className={styles.userInfo} onClick={setIsUserModalClicked}>
+                                        <FaRegCircleUser />
+                                        {/* 클릭시 DropdownMenu 노출 */}
+                                        {isModalOpen && <UserModal session={session} />}
+                                    </div>
                                 </div>
                             }
                         </>
                     </div>
                 </div>
             </nav>
+            {/* 모바일 반응형 메뉴 */}
+            <div className={`${styles.side_menu} ${isResponsiveOpen ? styles.side_menu_open : ''}`} onClick={handleHamburgerClick}>
+                <div className={styles['side_menu-header']}>
+                    {/* 네비게이션 로고 */}
+                    <div className={styles.logo}>
+                        <Link href={'/'}>
+                            <Image src={NavigationLogo} width={220.79} height={17} alt="어택 로고 이미지" />
+                        </Link>
+                    </div>
+                    {isHamburgerClicked ? (<MdOutlineCancel />) : (<GiHamburgerMenu />)}
+                </div>
+                {isResponsiveOpen && (
+                    <>
+                        {!session &&
+                            <div className={styles.side_menu_footer}>
+                                <AppLink
+                                    className={`${styles['create-btn']}`}
+                                    href={`/join/`}
+                                    label={'회원가입'}
+                                />
+                                <AppLink
+                                    className={styles['create-btn']}
+                                    href={'/login/'}
+                                    label={'로그인'}
+                                />
+                            </div>
+                        }
+
+                        {session?.user &&
+                            <>
+                                <div className={styles.side_menu_body}>
+                                    <div className={styles.side_menu_list}>
+                                        <Link className={styles.side_menu_link} href={'/'}>
+                                            링크1
+                                        </Link>
+                                        <Link className={styles.side_menu_link} href={'/'}>
+                                            링크2
+                                        </Link>
+                                        <Link className={styles.side_menu_link} href={'/'}>
+                                            링크3
+                                        </Link>
+                                        <Link className={styles.side_menu_link} href={'/'}>
+                                            링크4
+                                        </Link>
+                                        <Link className={styles.side_menu_link} href={'/'}>
+                                            링크5
+                                        </Link>
+                                        <button className={styles.side_menu_link}>
+                                            로그아웃
+                                        </button>
+                                    </div>
+                                </div>
+                            </>
+                        }
+                    </>
+                )}
+            </div>
         </header>
     );
 }
 
-export default GlobalNavbarComponent;
+export default GlobalNavbar;
